b373cd4ced71bb72c65647f5412230c99c08a90e
Luis Fernando Penha de Camargo <luisfernandopenhadecamargo@gmail.com>
2025-10-23 16:25:00 -0300

    fix: ajustando a posição da camera

--- three-word-scene.umd.js
+++ three-word-scene.umd.js
@@ -180,7 +180,9 @@
 
         scene.background = new THREE.Color(0xffffff);
 
-        const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+        const aspect = container.clientWidth / container.clientHeight;
+        const camera = new THREE.PerspectiveCamera(75, aspect, 0.1, 1000);
+        //const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
         camera.position.z = 100;
 
