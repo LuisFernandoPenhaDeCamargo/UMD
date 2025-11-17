beb1442387c9982a736cf50d8274a3797468df01
Luis Fernando Penha de Camargo <luisfernandopenhadecamargo@gmail.com>
2025-11-17 18:16:14 -0300

    já perdi a conta

--- three-word-scene.umd.js
+++ three-word-scene.umd.js
@@ -435,7 +435,7 @@ function animate({ controls, renderer, scene, camera }) {
     const delta = (now - last) / 1000; // segundos
     last = now;
 
-    t += delta * 0.5; // 0.5 = velocidade desejada (ajuste à vontade)
+    t += delta * 0.7; // 0.5 = velocidade desejada (ajuste à vontade)
 
     const s = (Math.sin(t) + 1) / 2; // 0 → 1 → 0
     const angle = angleA * (1 - s) + angleB * s;
